75c91467f8c0a06ff37173b736b2ee2775268a79
Divakar Velagacherla <divakarvelagacherla@gmail.com>
2025-08-29 19:05:14 -0400

    Registered POST endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"event-booking-rest-api-golang/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,13 +11,29 @@ func main() {
 	server := gin.Default()
 	server.GET("/events", getEvents)
 	server.GET("/", welcomePage)
+	server.POST("/events", createEvent)
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Get Events Page"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
 }
 
 func welcomePage(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "Home Page"})
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.BindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Couldnt parse the request"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
